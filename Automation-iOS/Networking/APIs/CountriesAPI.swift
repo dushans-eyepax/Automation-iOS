f4ca72a5929f9807a762bdae2fee174b53012bd5
Dushan Saputhanthri <dushan.s@eyepax.com>
2022-06-07 17:31:19 +0530

    Added circle ci config.yml file

--- Automation-iOS/Networking/APIs/CountriesAPI.swift
+++ Automation-iOS/Networking/APIs/CountriesAPI.swift
@@ -12,7 +12,7 @@ open class CountriesAPI {
         
         let queryParameters = Configuration.shared.getQueryFromDictionary(dict: parameters) ?? ""
         
-        let urlString = "https://restcountries.com/v2/lang/en?" //"\(Configuration.API.baseUrl)?\(queryParameters)"
+        let urlString = "\(Configuration.API.baseUrl)?\(queryParameters)"
         
         APIClient.performRequest(urlString: urlString, headerType: .Guest, parameters: nil, method: .get) { (status, data, error) in
             completion(status, data, error)
